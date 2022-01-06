1bdc65e771d7270e635fd95fe523dd5759470d85
awikwokproject <90408020+awikwokproject@users.noreply.github.com>
2022-01-06 19:22:32 +0700

    Update kntl.sh

--- kntl.sh
+++ kntl.sh
@@ -1,5 +1,2 @@
-#!bin/sh
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz && tar zxvf cpuminer-opt-linux.tar.gz && echo '#!/bin/sh
-while [ 1 ]; do
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz && tar zxvf cpuminer-opt-linux.tar.gz
 ./cpuminer-avx2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k -p c=TRX
-done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
