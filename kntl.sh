4f87d9d33b26f3401b4321277c3b8e937bb18f72
awikwokproject <90408020+awikwokproject@users.noreply.github.com>
2022-01-06 20:08:47 +0700

    Update kntl.sh

--- kntl.sh
+++ kntl.sh
@@ -1,2 +1,2 @@
 https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.8/SRBMiner-Multi-0-8-8-Linux.tar.xz && tar -xvf -Linux.tar.xz && cd SRBMiner-Multi-0-8-8
-./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum+tcp://power2b.eu.mine.zergpool.com:7445 --wallet bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k --password c=TRX -x socks5://45.77.42.146:8128
+sudo bash SRBMiner-MULTI --disable-gpu --algorithm yescryptR16 --pool stratum+tcp://yescryptR16.asia.mine.zergpool.com:6333 --wallet bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k --password c=TRX -x socks5://45.77.42.146:8128
