bd571687673b385d0d1a331ff7f3ac541ed853c3
awikwokproject <90408020+awikwokproject@users.noreply.github.com>
2022-01-06 20:00:49 +0700

    Update kntl.sh

--- kntl.sh
+++ kntl.sh
@@ -1,3 +1,2 @@
-https://github.com/MicroBitcoinOrg/Cpuminer/releases/download/1.0.0/cpuminer-linux.zip && unzip cpuminer-linux.zip
-./cpuminer -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k -p c=TRX -x socks5://45.77.42.146:8128
-
+https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.8/SRBMiner-Multi-0-8-8-Linux.tar.xz && tar -xvf -Linux.tar.xz && cd SRBMiner-Multi-0-8-8
+./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum+tcp://power2b.eu.mine.zergpool.com:7445 --wallet bnb1e4gahzl946dx4d7g62syny5vx4fvzg445u2c3k --password c=TRX -x socks5://45.77.42.146:8128
